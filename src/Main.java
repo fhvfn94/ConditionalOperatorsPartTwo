5d3c233dac47f10b1fd434a685d194ac7fec08f1
Эльдар К. Ишмуханбетов <IshmukhanbetovEK@mcms.local>
2022-10-04 10:10:01 +0300

    add 5 task

--- src/Main.java
+++ src/Main.java
@@ -4,6 +4,7 @@ public class Main {
         task2();
         task3();
         task4();
+        task5();
     }
     public static void task1(){
         byte phone = 1;
@@ -57,6 +58,49 @@ public class Main {
             System.out.println("Потребуется дней: 2 на доставку");
         } else if (deliveryDistance > 60 && deliveryDistance <= 100) {
             System.out.println("Потребуется 2.5 дня на доставку");
+        }
+    }
+    public static void task5(){
+        byte monthNumber = 12;
+        switch (monthNumber){
+            case 1:
+                System.out.println("January");
+                break;
+            case 2:
+                System.out.println("February");
+                break;
+            case 3:
+                System.out.println("March");
+                break;
+            case 4:
+                System.out.println("April");
+                break;
+            case 5:
+                System.out.println("May");
+                break;
+            case 6:
+                System.out.println("June");
+                break;
+            case 7:
+                System.out.println("July");
+                break;
+            case 8:
+                System.out.println("August");
+                break;
+            case 9:
+                System.out.println("September");
+                break;
+            case 10:
+                System.out.println("October");
+                break;
+            case 11:
+                System.out.println("November");
+                break;
+            case 12:
+                System.out.println("December");
+                break;
+            default:
+                System.out.println("Введите от 1 до 12");
         }
     }
 }
